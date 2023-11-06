c5ce2ed4a93470face16848d5e36236c6814c550
Sreelaxmi12 <sreelaxmi0803@gmail.com>
2023-11-06 12:25:41 +0530

    Updated calculator

--- src/App.js
+++ src/App.js
@@ -1,55 +1,55 @@
 import { useReducer } from "react";
 import DigitButton from "./components/DigitButton";
 import OperationButton from "./components/OperationButton";
-import "./styles.css"
+import "./styles.css";
 
 export const ACTIONS = {
-  ADD_DIGIT: 'add-digit',
-  CHOOSE_OPERATION: 'choose-operation',
-  CLEAR: 'clear',
-  DELETE_DIGIT: 'delete-digit',
-  EVALUATE: 'evaluate'
-}
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate",
+};
 
-function reducer(state, {type, payload}) {
-  switch(type){
+function reducer(state, { type, payload }) {
+  switch (type) {
     case ACTIONS.ADD_DIGIT:
-      if(state.overwrite){
-        return{
+      if (state.overwrite) {
+        return {
           ...state,
           currentOpearnd: payload.digit,
           overwrite: false,
-        }
+        };
       }
-      if (payload.digit === "0" && state.currentOperand === "0"){
-        return state
+      if (payload.digit === "0" && state.currentOperand === "0") {
+        return state;
       }
       if (payload.digit === "." && state.currentOperand.includes(".")) {
-        return state
+        return state;
       }
-      return{
+      return {
         ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`,
-      }
-    
+      };
+
     case ACTIONS.CHOOSE_OPERATION:
-      if(state.currentOperand == null && state.previousOperand == null){
-        return state
+      if (state.currentOperand == null && state.previousOperand == null) {
+        return state;
       }
 
-      if(state.currentOperand == null){
+      if (state.currentOperand == null) {
         return {
           ...state,
           operation: payload.operation,
-        }
+        };
       }
-      if(state.previousOperand == null){
+      if (state.previousOperand == null) {
         return {
           ...state,
           operation: payload.operation,
-          previousOperand : state.currentOperand,
+          previousOperand: state.currentOperand,
           currentOperand: null,
-        }
+        };
       }
       return {
         ...state,
@@ -59,43 +59,47 @@ function reducer(state, {type, payload}) {
       };
 
     case ACTIONS.CLEAR:
-      return {} 
-      
+      return {};
+
     case ACTIONS.DELETE_DIGIT:
-      if(state.overwrite){
+      if (state.overwrite) {
         return {
           ...state,
           overwrite: false,
-          currentOperand: null
-        }
+          currentOperand: null,
+        };
       }
-      if(state.currentOperand == null) return state
-      if(state.currentOperand.length === 1){
-        return { ...state, currentOperand: null }
+      if (state.currentOperand == null) return state;
+      if (state.currentOperand.length === 1) {
+        return { ...state, currentOperand: null };
       }
       return {
         ...state,
-        currentOperand: state.currentOperand.slice(0, -1)
-      }
+        currentOperand: state.currentOperand.slice(0, -1),
+      };
 
     case ACTIONS.EVALUATE:
-      if(state.operation == null || state.currentOperand == null || state.previousOperand == null){
-        return state
+      if (
+        state.operation == null ||
+        state.currentOperand == null ||
+        state.previousOperand == null
+      ) {
+        return state;
       }
-      return{
+      return {
         ...state,
         previousOperand: null,
         operation: null,
         currentOperand: evaluate(state),
-      }  
+      };
   }
 }
 
-function evaluate({ currentOperand, previousOperand, operation}){
-  const prev = parseFloat(previousOperand)
-  const current = parseFloat(currentOperand)
-  if(isNaN(prev) || isNaN(current)) return ""
-  let computation = ""
+function evaluate({ currentOperand, previousOperand, operation }) {
+  const prev = parseFloat(previousOperand);
+  const current = parseFloat(currentOperand);
+  if (isNaN(prev) || isNaN(current)) return "";
+  let computation = "";
   switch (operation) {
     case "+":
       computation = prev + current;
@@ -110,21 +114,25 @@ function evaluate({ currentOperand, previousOperand, operation}){
       computation = prev / current;
       break;
   }
-  return computation.toString()
+  return computation.toString();
 }
 
-const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {maximumFractionDigits: 0,})
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
+  maximumFractionDigits: 0,
+});
 
-function formatOperand(operand){
-  if(operand == null) return 
-  const [integer, decimal] = operand.split(".")
-  if(decimal == null) return INTEGER_FORMATTER.format(integer)
+function formatOperand(operand) {
+  if (operand == null) return;
+  const [integer, decimal] = operand.split(".");
+  if (decimal == null) return INTEGER_FORMATTER.format(integer);
   return `${INTEGER_FORMATTER.format(integer)}.${decimal}`;
 }
 
-
 function App() {
-  const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {})
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
   return (
     <>
       <div className="calculator-grid">
